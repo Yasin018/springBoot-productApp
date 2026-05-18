7c9ead66ab95af1a77e3c0476f207b25cc3183f4
Yasin <143324300+Yasin018@users.noreply.github.com>
2026-05-18 22:58:00 +0530

    Refactor IProductService to include ProductDTO and queries
    Updated IProductService to use ProductDTO and added custom querying methods.

--- ProductApp/src/main/java/com/productApp/Service/IProductService.java
+++ ProductApp/src/main/java/com/productApp/Service/IProductService.java
@@ -2,14 +2,31 @@ package com.productApp.Service;
 
 import java.util.List;
 
+import com.productApp.Exceptions.ProductNotFoundException;
 import com.productApp.Model.Product;
+import com.productApp.Model.ProductDTO;
 
 public interface IProductService {
 	
-	void addProduct(Product productName);
-	void updateProduct(Product productName);
+	void addProduct(ProductDTO productDTO);
+	void updateProduct(ProductDTO productDTO);
 	void deleteProduct(int productId);
-	List<Product> getAllProducts();
-	Product getById(int id);
+	List<ProductDTO> getAllProducts();
+	ProductDTO getById(int id);
 	
+	//derived querying
+	public List<ProductDTO> getByBrand(String brand) throws ProductNotFoundException;
+	public List<ProductDTO> getByBrandContains(String brand, String productName) throws ProductNotFoundException;
+	
+	//custom querying
+	public List<ProductDTO> getByCat(String brand) throws ProductNotFoundException;
+	public List<ProductDTO> getByBrandPriceLess(String brand, double price) throws ProductNotFoundException;
+	
+	//native querying
+	public List<ProductDTO> getByLesserPrice(double price) throws ProductNotFoundException;
+	public List<ProductDTO> getByCategoryName(String category, String product) throws ProductNotFoundException;
+	
+	//named querying
+	public List<ProductDTO> getProductByBrand(String brand) throws ProductNotFoundException;
+	public List<ProductDTO> getByCatPriceLess(String category, double price) throws ProductNotFoundException;
 }
