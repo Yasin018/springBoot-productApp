2895967da2da6ddfc77911cbeb2d44d73e5fbd41
Yasin <143324300+Yasin018@users.noreply.github.com>
2026-05-18 22:59:21 +0530

    Comment out product service method calls
    Commented out multiple product service calls for testing.

--- ProductApp/src/main/java/com/productApp/ProductAppApplication.java
+++ ProductApp/src/main/java/com/productApp/ProductAppApplication.java
@@ -20,15 +20,53 @@ public class ProductAppApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		Product product = new Product("Mobile","Realme","Electronics",23000);
-		//productService.addProduct(product);
-		productService.updateProduct(product);
+//		Product product = new Product("Microphone","Moana","Electronics",20000);
+//		productService.addProduct(product);
+//		product = new Product("Laptop","HP","Electronics",73000);
+//		productService.addProduct(product);
+//		Product product1 = new Product("Ear phone","Samsung","Electronics",600);
+//		productService.addProduct(product1);
+//		Product product2 = new Product("Speaker","Samsung","Electronics",15000);
+//		productService.addProduct(product2);
+//		Product product3 = new Product("Head phone","Boat","Electronics",13000);
+//		productService.addProduct(product3);
+//		Product product4 = new Product("Pencils","Camlin","Stationary",200);
+//		productService.addProduct(product4);
+//		Product product5 = new Product("Pen","Classmate","Stationary",300);
+//		productService.addProduct(product5);
+//		Product product6 = new Product("Color Pencils","Camlin","Stationary",200);
+//		productService.addProduct(product6);
+		//productService.updateProduct(product);
 		
 		System.out.println();
-		System.out.println(productService.getAllProducts());
+		//System.out.println(productService.getAllProducts());
+//		
+//		System.out.println();
+//		System.out.println(productService.getById(1));
 		
-		System.out.println();
-		System.out.println(productService.getById(1));
+//		System.out.println("Get By Brand");
+//		System.out.println(productService.getByBrand("HP"));
+//		
+//		System.out.println("Get By Brand And Name containing");
+//		System.out.println(productService.getByBrandContains("Camlin","pen"));
+		
+//		System.out.println("Get By Category");
+//		productService.getByCat("Electronics").forEach(System.out::println);
+//		
+//		System.out.println("Get By Brand And Price Less Than");
+//		productService.getByBrandPriceLess("Camlin",600).forEach(System.out::println);
+		
+//		System.out.println("Get By Lesser Price:");
+//		productService.getByLesserPrice(30000).forEach(System.out::println);
+//		
+//		System.out.println("Get By Category & name contains:");
+//		productService.getByCategoryName("Electronics","Mo").forEach(System.out::println);
+		
+		System.out.println("Get Product By Brand:");
+		productService.getProductByBrand("Camlin").forEach(System.out::println);
+		
+		System.out.println("Get By Category & price less than:");
+		productService.getByCatPriceLess("Electronics",13000).forEach(System.out::println);
 	}
 
 }
